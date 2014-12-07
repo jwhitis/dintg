2ab95058ccee1477426744c830183252c8b53c43
jwhitis <jack.whitis@gmail.com>
2014-12-07 01:35:40 -0600

    Refactor calendar facade

--- app/domain/calendar_facade.rb
+++ app/domain/calendar_facade.rb
@@ -1,24 +1,23 @@
 class CalendarFacade
-  attr_accessor :user, :client
 
   def initialize(user)
-    self.user = user
-    self.client = new_client
-    client.authorization.access_token = user.token.fresh_token
+    @user = user
+    @client = client
+    @client.authorization.access_token = @user.token.fresh_token
   end
 
   def list_calendars
-    result = client.execute(
-      api_method: calendar.calendar_list.list
+    result = @client.execute(
+      api_method: calendar_api.calendar_list.list
     )
 
     result.data.items
   end
 
   def create_event(event_params)
-    result = client.execute(
-      api_method: calendar.events.insert,
-      parameters: { "calendarId" => user.configuration.calendar_id },
+    result = @client.execute(
+      api_method: calendar_api.events.insert,
+      parameters: { "calendarId" => @user.configuration.calendar_id },
       body: event_params.to_json,
       headers: { "Content-Type" => "application/json" }
     )
@@ -27,10 +26,10 @@ class CalendarFacade
   end
 
   def find_conflicts(gig)
-    result = client.execute(
-      api_method: calendar.events.list,
+    result = @client.execute(
+      api_method: calendar_api.events.list,
       parameters: {
-        "calendarId" => user.configuration.calendar_id,
+        "calendarId" => @user.configuration.calendar_id,
         "timeMin" => gig.starts_at.iso8601,
         "timeMax" => gig.ends_at.iso8601
       }
@@ -41,15 +40,15 @@ class CalendarFacade
 
   private
 
-  def new_client
+  def calendar_api
+    @calendar_api ||= @client.discovered_api("calendar", "v3")
+  end
+
+  def client
     Google::APIClient.new(
       application_name: "Do I Need This Gig?",
       application_version: "v1"
     )
-  end
-
-  def calendar
-    client.discovered_api("calendar", "v3")
   end
 
 end
