153941678ff945a01ef559d7a1b2b9ee93b1d8e2
jwhitis <jack.whitis@gmail.com>
2014-12-22 16:44:13 -0600

    Add flip animation to calendar days

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -5,18 +5,24 @@ $(document).on("ready page:load", function() {
   $("#wrapper").css("min-height", page_height);
 
   // Set background color of calendar days by event density
-  $("#calendar .day").not(".disabled").each(function() {
+  $("#calendar .front").not(".disabled").each(function() {
     var background = dayBackground($(this), "53%");
     $(this).css("background-color", background);
   });
 
-  // Clicking calendar day darkens background color
-  $("#calendar .day").not(".disabled").mousedown(function(event) {
+  // Clicking front of calendar day triggers forward animation
+  $("#calendar .front").not(".disabled").mousedown(function(event) {
     var background = dayBackground($(this), "48%");
     $(this).css("background-color", background);
   }).mouseup(function() {
     var background = dayBackground($(this), "53%");
     $(this).css("background-color", background);
+    $(this).parent(".flipper").addClass("flip");
+  });
+
+  // Clicking back of calendar day triggers reverse animation
+  $("#calendar .back").not(".disabled").click(function() {
+    $(this).parent(".flipper").removeClass("flip");
   });
 
   function dayBackground(element, lightness) {
