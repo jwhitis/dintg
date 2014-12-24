7bda12868e91de62a6d68180e780d9048bc46d33
jwhitis <jack.whitis@gmail.com>
2014-12-23 23:32:05 -0600

    Clicking calendar day flips entire calendar instead of just day

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -5,24 +5,24 @@ $(document).on("ready page:load", function() {
   $("#wrapper").css("min-height", page_height);
 
   // Set background color of calendar days by event density
-  $("#calendar .front").not(".disabled").each(function() {
+  $("#calendar td").not(".disabled").each(function() {
     var background = dayBackground($(this), "53%");
     $(this).css("background-color", background);
   });
 
-  // Clicking front of calendar day triggers forward animation
-  $("#calendar .front").not(".disabled").mousedown(function(event) {
+  // Clicking calendar day triggers flip animation
+  $("#calendar td").not(".disabled").mousedown(function(event) {
     var background = dayBackground($(this), "48%");
     $(this).css("background-color", background);
   }).mouseup(function() {
     var background = dayBackground($(this), "53%");
     $(this).css("background-color", background);
-    $(this).parent(".flipper").addClass("flip");
+    $(".flipper").addClass("flip");
   });
 
-  // Clicking back of calendar day triggers reverse animation
-  $("#calendar .back").not(".disabled").click(function() {
-    $(this).parent(".flipper").removeClass("flip");
+  // Clicking back of calendar triggers reverse flip animation
+  $("#calendar .back").click(function() {
+    $(".flipper").removeClass("flip");
   });
 
   function dayBackground(element, lightness) {
