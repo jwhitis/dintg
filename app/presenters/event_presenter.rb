0f30f1bfceff1d35b9fe57b472117e75a6f2cbfa
jwhitis <jack.whitis@gmail.com>
2014-12-25 12:53:38 -0600

    Finish styling calendar back

--- app/presenters/event_presenter.rb
+++ app/presenters/event_presenter.rb
@@ -5,21 +5,22 @@ class EventPresenter
   end
 
   def event_list
-    return no_events if @events.empty?
-    html = @events.reduce("") { |html, event| html + event_html(event) }
-    helpers.content_tag(:ul, html, { class: "event-list" }, false)
+    if @events.empty?
+      helpers.content_tag(:p, "You're wide open this day.", class: "no-events")
+    else
+      html = @events.reduce("") { |html, event| html + event_html(event) }
+      helpers.content_tag(:ul, html, { class: "event-list" }, false)
+    end
   end
 
   private
 
-  def no_events
-    helpers.content_tag(:p, "You're wide open that day.", class: "no-events")
-  end
-
   def event_html(event)
     helpers.content_tag(:li, class: "event") do
       helpers.content_tag(:div, class: "row") do
-        helpers.content_tag(:div, event.summary, class: "col-xs-6") +
+        helpers.content_tag(:div, class: "col-xs-6") do
+          helpers.content_tag(:i, "", class: "fa fa-calendar-o") + event.summary
+        end +
         helpers.content_tag(:div, format_event_time(event), class: "col-xs-6")
       end
     end
