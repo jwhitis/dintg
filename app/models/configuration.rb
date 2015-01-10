c2877a96aaeab75c074e0b9da33438fa368cbf20
jwhitis <jack.whitis@gmail.com>
2015-01-09 18:10:07 -0600

    Monthly goal is only required on account update

--- app/models/configuration.rb
+++ app/models/configuration.rb
@@ -4,7 +4,8 @@ class Configuration < ActiveRecord::Base
   belongs_to :user
 
   validates_numericality_of :monthly_goal, greater_than: 0, less_than: 1000000,
-    message: "Please enter a monthly goal that is more than $0.00 and less than $1,000,000.00."
+    message: "Please enter a monthly goal that is more than $0.00 and less than $1,000,000.00.",
+    on: :update
   validates_inclusion_of :time_period, in: TimeRangeDictionary::TIME_PERIODS
 
   def formatted_monthly_goal(unit = "$")
