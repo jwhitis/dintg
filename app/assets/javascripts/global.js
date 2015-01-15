2078e8ac47c4dbbe7e92a04c8a6ebe8c6d16a691
jwhitis <jack.whitis@gmail.com>
2015-01-14 18:34:02 -0600

    Move JS for calendar into separate file

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -13,59 +13,9 @@ $(document).on("ready page:load", function() {
     }, 3000);
   }
 
-  // Set background color of calendar days by event density
-  $("#calendar td").not(".disabled").each(function() {
-    var background = dayBackground($(this), "53%");
-    $(this).css("background-color", background);
+  // Clicking item in gig list flips item
+  $(".gig-list li").click(function() {
+    $(this).find(".flipper").toggleClass("flip");
   });
-
-  // Clicking calendar day triggers animation
-  $("#calendar td").not(".disabled").mousedown(function(event) {
-    var background = dayBackground($(this), "48%");
-    $(this).css("background-color", background);
-  }).mouseup(function() {
-    var background = dayBackground($(this), "53%");
-    $(this).css("background-color", background);
-
-    // Render event list
-    var event_list = $(this).data("event-list");
-    if (event_list) {
-      $("#events").html(event_list);
-    } else {
-      $("#events").html("<p class='no-events'>You're wide open this day.</p>");
-    }
-
-    // Set "Add Event" button path
-    var path = $(this).data("path");
-    $("#add-event").attr("href", path);
-
-    // Set event list title
-    var title = $(this).data("title");
-    $("#events-title").text(title);
-
-    $(".flipper").addClass("flip");
-  });
-
-  // Clicking exit button triggers reverse animation
-  $("#calendar .exit").click(function() {
-    $(".flipper").removeClass("flip");
-  });
-
-  // Loading icon appears on calendar reload
-  $("#calendar").find(".previous, .next, .disabled a").click(function() {
-    $(".loading").show();
-  });
-
-  // Clicking toggle in gig list flips gig
-  $(".gig-list .flip-toggle").click(function() {
-    $(this).parents(".flipper").toggleClass("flip");
-  });
-
-  function dayBackground(element, lightness) {
-    var hue = 33;
-    var density = +element.data("density");
-    hue = Math.round(hue - (hue * density));
-    return "hsla(" + hue + ", 100%, " + lightness + ", .85)";
-  }
 
 });
