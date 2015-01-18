9b6220e3510cba3167df240698edec6a687e24c0
jwhitis <jack.whitis@gmail.com>
2015-01-18 14:42:50 -0600

    Add collapsible navbar; set maximum container width

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -13,6 +13,15 @@ $(document).on("ready page:load", function() {
     }, 3000);
   }
 
+  // Clicking bars icon toggles navbar
+  $(".nav-toggle").click(function() {
+    if ($("nav").is(":hidden")) {
+      $("nav").slideDown(600, "easeOutBounce");
+    } else {
+      $("nav").slideUp(600, "easeOutQuint");
+    }
+  });
+
   // Clicking item in gig list flips item
   $(".gig-list li").click(function(event) {
     if (event.target.tagName != "A") {
