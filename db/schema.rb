7539a23b6d3e1b4a9130cb49a371eaee623206e3
jwhitis <jack.whitis@gmail.com>
2015-04-26 13:02:28 -0500

    Add time_zone column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141207163645) do
+ActiveRecord::Schema.define(version: 20150426145859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 20141207163645) do
     t.string   "uid"
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "time_zone"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
